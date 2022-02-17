45be1c52f1b132f511e0d28434b8427c929c7c9d
balezz <balib@rambler.cu>
2022-02-17 12:49:16 +0300

    Add AJAX button processing

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 # configuration
 
@@ -14,6 +14,12 @@ app.config.from_object(__name__)
 def index():
     quest = {'quest': 'Дайте определение войны.'}
     return render_template('index.html', quest=quest)
+
+
+@app.route('/wave', methods=['POST'])
+def wave():
+    print('Get wav file')
+    return json.dumps({'status': 'OK'})
 
 
 if __name__ == "__main__":
