1ccb0dc3989a5f8ed00f6c90dce344b79db1480c
balezz <balib@rambler.cu>
2022-03-12 00:09:10 +0300

    goto vosk small model

--- project/engine.py
+++ project/engine.py
@@ -1,23 +1,40 @@
-from pathlib import Path
-from tools import data_preparator, segmenter, recognizer, transcriptions_parser
-from tools.utils import make_ass, make_wav_scp, delete_folder
+import sox
+import wave
+from vosk import Model, KaldiRecognizer, SetLogLevel
 
 
-def recognize(temp, wav):
-    wav_scp = str(Path(temp) / 'wav.scp')
-    make_wav_scp(wav, wav_scp)
-    segm = segmenter.Segmenter(wav_scp,
-                               './model/final.raw',
-                               './model/conf/post_output.vec',
-                               './model/conf/mfcc_hires.conf',
-                               temp)
-    segments = segm.segment()
-    wav_segments_scp, utt2spk, spk2utt = segm.extract_segments(segments)
-    rec = recognizer.Recognizer(wav_segments_scp, '../model/final.mdl', '../model/HCLG.fst', '../model/words.txt',
-                                '../model/conf/mfcc.conf', '../model/conf/ivector_extractor.conf', spk2utt, temp)
-    transcriptions = rec.recognize(Path(wav).stem)
-    ass = str(Path(temp) / 'wav.ass')
-    make_ass(Path(wav).name, segments, transcriptions, utt2spk, ass)
-    pars = transcriptions_parser.TranscriptionsParser('', '', '', 0, 0, 'wav.csv')
-    transcriptions_df = pars.process_file(ass)
-    return transcriptions_df
+def vosk_decode(wave_path):
+    SetLogLevel(0)
+
+    wf = wave.open(wave_path, "rb")
+
+    if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+        print("Audio file must be WAV format mono PCM.")
+        exit(1)
+
+    model = Model("vosk-model-small")
+    rec = KaldiRecognizer(model, wf.getframerate())
+    rec.SetWords(True)
+
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
+
+    print(rec.FinalResult())
+
+
+if __name__ == '__main__':
+    wav_path = 'waves/war_1.wav'
+    wav_info = sox.file_info.info(wav_path)
+    info = {}
+    info['Длительность аудио'] = str(round(wav_info['duration'], 2)) + ' с'
+    info['Число каналов'] = wav_info['channels']
+    info['Частота дискретизации'] = str(int(wav_info['sample_rate'])) + ' Гц'
+    print(info)
+
+    vosk_decode(wav_path)
